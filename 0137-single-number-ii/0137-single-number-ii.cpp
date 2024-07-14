a9fa08e70636f558ffa3b17f9ba8b4fe79550ea6
Aditya Kumar Singh <128071145+AdityaKrSingh26@users.noreply.github.com>
2024-07-14 23:13:24 +0530

    Time: 5 ms (65.29%), Space: 11.8 MB (58.72%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,16 +2,23 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) 
     {
+        // ======== concept of buckets ===========
+        int ones=0, twos=0;
+        for(int i = 0;i<nums.size();i++){
+            ones = (ones^nums[i]) & (~twos);
+            twos = (twos^nums[i]) & (~ones);
+        }
+        return ones;
         
-        // bit solution 
-        int ans =0;
-        for(int i = 0; i<32 ; i++){
-            int cnt =0;
-            for(int j = 0; j<nums.size() ; j++)
-                if(nums[j] & (1<<i)) cnt++;
-            if(cnt%3!=0)
-                ans = ans | (1<<i);
-        } 
-        return ans;
+        // ======== bit solution =================
+        // int ans =0;
+        // for(int i = 0; i<32 ; i++){
+        //     int cnt =0;
+        //     for(int j = 0; j<nums.size() ; j++)
+        //         if(nums[j] & (1<<i)) cnt++;
+        //     if(cnt%3!=0)
+        //         ans = ans | (1<<i);
+        // } 
+        // return ans;
     }
 };
