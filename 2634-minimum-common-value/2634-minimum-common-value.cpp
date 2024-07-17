9fdb12d17786196176aaa6f5f1386fffe561db18
Aditya Kumar Singh <128071145+AdityaKrSingh26@users.noreply.github.com>
2024-07-17 23:42:15 +0530

    Time: 78 ms (27.93%), Space: 53.3 MB (27.49%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.cpp
+++ 2634-minimum-common-value/2634-minimum-common-value.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        int mins = INT_MAX;
-        unordered_map <int,bool> mpp;
-        for(int num:nums1)
-            mpp[num] = true;
-        for(int num:nums2)
-        {
-            if(mpp.count(num)>0 && mpp[num] == true)
-                mins = min(mins , num);
-            mpp[num] = false;
+
+        int maxi = 0 ,i = 0 ,j = 0;
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
+                maxi = nums1[i];
+                return maxi;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
         }
-        if(mins==INT_MAX)
-            return -1;
-        return mins;
+        return -1;
+
+
+        // int mins = INT_MAX;
+        // unordered_map <int,bool> mpp;
+        // for(int num:nums1)
+        //     mpp[num] = true;
+        // for(int num:nums2)
+        // {
+        //     if(mpp.count(num)>0 && mpp[num] == true)
+        //         mins = min(mins , num);
+        //     mpp[num] = false;
+        // }
+        // if(mins==INT_MAX)
+        //     return -1;
+        // return mins;
     }
 };
