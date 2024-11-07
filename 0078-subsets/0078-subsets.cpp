47c9f7063db253d3aca5cd9ba121a72ffc39ec87
Aditya Kumar Singh <128071145+AdityaKrSingh26@users.noreply.github.com>
2024-11-07 10:07:27 +0530

    Time: 4 ms (13.07%), Space: 13.8 MB (29.12%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        int n = nums.size();
-        int subset = 1<<n;
-        for(int i = 0; i <subset ; i++)
-        {
-            vector<int> curr;
-            for(int j = 0; j < n; j++)
-            {
-                if(i & (1<<j))
-                {    
-                    curr.push_back(nums[j]);
-                }
-            }
-            ans.push_back(curr);
+    void createSubSec(vector<int> nums, int index , vector<vector<int>>& res, vector<int>& ds)
+    {
+        if(index==nums.size()){
+            res.push_back(ds);
+            return;
         }
-        return ans;
+
+        ds.push_back(nums[index]);
+        createSubSec(nums,index+1,res,ds);
+        ds.pop_back();
+        createSubSec(nums,index+1,res,ds);
     }
 
+    vector<vector<int>> subsets(vector<int>& nums) {
+        // --------------- Recursion ---------------------
+        vector<vector<int>> res;
+        vector<int> ds;
+        int index=0;
+        createSubSec(nums,index,res,ds);
+        return res;
+
+        // ----------- Bit Manipulation ------------------
+        // vector<vector<int>> ans;
+        // int n = nums.size();
+        // int subset = 1<<n;
+        // for(int i = 0; i <subset ; i++)
+        // {
+        //     vector<int> curr;
+        //     for(int j = 0; j < n; j++)
+        //     {
+        //         if(i & (1<<j))
+        //         {
+        //             curr.push_back(nums[j]);
+        //         }
+        //     }
+        //     ans.push_back(curr);
+        // }
+        // return ans;
+    }
 };
