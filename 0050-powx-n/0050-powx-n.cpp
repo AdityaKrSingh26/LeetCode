62ee2c9c2f1493263c8dfc502f2cd37b9a5f6aca
Aditya Kumar Singh <128071145+AdityaKrSingh26@users.noreply.github.com>
2024-11-09 19:41:42 +0530

    Time: 0 ms (100%), Space: 8.4 MB (40.37%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
-        if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
-        double num = 1;
-        if(n>=0){
-            while(n>0){
-                num *= x;
-                n--;
-            }
+        // ===========Recursion=================
+        if (n == 0)
+            return 1;
+        if (n <= 0) {
+            n = abs(n);
+            x = 1 / x;
         }
-        else{
-            n = -n;
-            while(n>0){
-                num *= x;
-                n--;
-            }
-            num = 1.0/num;
+        if (n % 2 == 0) {
+            return myPow(x * x, n / 2);
+            // for even x^n = (x^2)^n/2
+        } else {
+            return x * myPow(x, n - 1);
+            // for odd x^n = x * x^n-1
         }
-        return num;
+
+        // ============Loop=====================
+        // if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
+        // if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
+        // double num = 1;
+        // if(n>=0){
+        //     while(n>0){
+        //         num *= x;
+        //         n--;
+        //     }
+        // }
+        // else{
+        //     n = -n;
+        //     while(n>0){
+        //         num *= x;
+        //         n--;
+        //     }
+        //     num = 1.0/num;
+        // }
+        // return num;
     }
 };
