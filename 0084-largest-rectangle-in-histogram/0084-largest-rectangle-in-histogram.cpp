9d44aaa1e5ae778479741397858cc8a6acae12b6
Aditya Kumar Singh <128071145+AdityaKrSingh26@users.noreply.github.com>
2024-12-08 13:09:18 +0530

    Time: 19 ms (88.39%), Space: 81.4 MB (57.56%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,37 +1,62 @@
 class Solution {
 public:
-    void findPse(vector<int>& pse, vector<int>& heights) {
+    // ==== Optimal ======
+    int largestRectangleArea(vector<int>& heights){
         stack<int> st;
-        for (int i = 0; i < heights.size(); i++) {
-            while (!st.empty() && heights[st.top()] >= heights[i]) {
+        int maxi = 0,nse,pse,element;
+        for(int i =0; i<heights.size();i++){
+            while(!st.empty() && heights[st.top()]>heights[i]){
+                element = st.top();
                 st.pop();
+                nse = i;
+                pse = st.empty()?-1:st.top();
+                maxi = max(maxi , heights[element]*(nse-pse-1));
             }
-            pse.push_back(st.empty() ? -1 : st.top());
-            st.push(i); // Push index instead of height
+            st.push(i);
         }
-    }
-
-    void findNse(vector<int>& nse, vector<int>& heights) {
-        stack<int> st;
-        nse.resize(heights.size());
-        for (int i = heights.size() - 1; i >= 0; i--) {
-            while (!st.empty() && heights[st.top()] >= heights[i]) {
-                st.pop();
-            }
-            nse[i] = st.empty() ? heights.size() : st.top();
-            st.push(i); // Push index instead of height
-        }
-    }
-
-    int largestRectangleArea(vector<int>& heights) {
-        vector<int> nse, pse;
-        int maxi = 0;
-        findNse(nse, heights);
-        findPse(pse, heights);
-        for (int i = 0; i < heights.size(); i++) {
-            int width = nse[i] - pse[i] - 1;
-            maxi = max(maxi, heights[i] * width);
+        while(!st.empty()){
+            nse = heights.size();
+            element = st.top();
+            st.pop();
+            pse = st.empty()?-1:st.top();
+            maxi = max(maxi , heights[element]*(nse-pse-1));
         }
         return maxi;
     }
+
+    // ==== Brute force ==== 
+    // void findPse(vector<int>& pse, vector<int>& heights) {
+    //     stack<int> st;
+    //     for (int i = 0; i < heights.size(); i++) {
+    //         while (!st.empty() && heights[st.top()] >= heights[i]) {
+    //             st.pop();
+    //         }
+    //         pse.push_back(st.empty() ? -1 : st.top());
+    //         st.push(i); // Push index instead of height
+    //     }
+    // }
+
+    // void findNse(vector<int>& nse, vector<int>& heights) {
+    //     stack<int> st;
+    //     nse.resize(heights.size());
+    //     for (int i = heights.size() - 1; i >= 0; i--) {
+    //         while (!st.empty() && heights[st.top()] >= heights[i]) {
+    //             st.pop();
+    //         }
+    //         nse[i] = st.empty() ? heights.size() : st.top();
+    //         st.push(i); // Push index instead of height
+    //     }
+    // }
+
+    // int largestRectangleArea(vector<int>& heights) {
+    //     vector<int> nse, pse;
+    //     int maxi = 0;
+    //     findNse(nse, heights);
+    //     findPse(pse, heights);
+    //     for (int i = 0; i < heights.size(); i++) {
+    //         int width = nse[i] - pse[i] - 1;
+    //         maxi = max(maxi, heights[i] * width);
+    //     }
+    //     return maxi;
+    // }
 };
