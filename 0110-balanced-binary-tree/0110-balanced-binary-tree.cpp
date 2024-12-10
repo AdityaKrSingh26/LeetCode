ab61674843f2e6b0f6eeb279070ac21f9aeb2b77
Aditya Kumar Singh <128071145+AdityaKrSingh26@users.noreply.github.com>
2024-12-10 14:25:11 +0530

    Time: 0 ms (100%), Space: 23 MB (16.83%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -6,28 +6,46 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-        if(!root)
+    int checkHeight(TreeNode* root) {
+        if (!root)
             return 0;
-        int l = maxDepth(root->left);
-        int r = maxDepth(root->right);
-        return (1+max(l,r));
+        int leftHeight = checkHeight(root->left);
+        int rightHeight = checkHeight(root->right);
+        if (leftHeight == -1 || rightHeight == -1)
+            return -1;
+        if (abs(leftHeight - rightHeight) > 1)
+            return -1; 
+        return 1 + max(leftHeight, rightHeight);
     }
 
-    bool isBalanced(TreeNode* root) {
-        if(!root)
-            return true;
-        int left = maxDepth(root->left);
-        int right = maxDepth(root->right);
-        cout << left - right <<endl;
-        if (abs(left - right) <= 1 && isBalanced(root->left) && isBalanced(root->right)) {
-            return true;
-        }
-        return false;
+    bool isBalanced(TreeNode* root) { 
+        return checkHeight(root) != -1; 
     }
+
+    // ====== Brute Force ====
+    // int maxDepth(TreeNode* root) {
+    //     if (!root)
+    //         return 0;
+    //     int l = maxDepth(root->left);
+    //     int r = maxDepth(root->right);
+    //     return (1 + max(l, r));
+    // }
+    // bool isBalanced(TreeNode* root) {
+    //     if (!root)
+    //         return true;
+    //     int left = maxDepth(root->left);
+    //     int right = maxDepth(root->right);
+    //     cout << left - right << endl;
+    //     if (abs(left - right) <= 1 && isBalanced(root->left) &&
+    //         isBalanced(root->right)) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
 };
