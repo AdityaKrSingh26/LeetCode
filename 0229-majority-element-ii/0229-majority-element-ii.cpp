e6613b969804983df358ef5f9cc94979073f1af6
Aditya Kumar Singh <128071145+AdityaKrSingh26@users.noreply.github.com>
2024-12-21 13:10:46 +0530

    Time: 0 ms (100%), Space: 19.9 MB (20.68%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,14 +1,60 @@
 class Solution {
 public:
+    // ===== Boyer-Moore Majority Voting Solution =====
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> a;
+        int count1 = 0, count2 = 0; // Counters for the potential majority elements
+        int candidate1 = 0, candidate2 = 0; // Potential majority element candidates
+
+        // First pass to find potential majority elements.
         for (int i = 0; i < nums.size(); i++) {
-            a[nums[i]]++;
+            // If count1 is 0 and the current number is not equal to candidate2, update candidate1.
+            if (count1 == 0 && nums[i] != candidate2) {
+                count1 = 1;
+                candidate1 = nums[i];
+            } 
+            // If count2 is 0 and the current number is not equal to candidate1, update candidate2.
+            else if (count2 == 0 && nums[i] != candidate1) {
+                count2 = 1;
+                candidate2 = nums[i];
+            } 
+            // Update counts for candidate1 and candidate2.
+            else if (candidate1 == nums[i]) {
+                count1++;
+            } else if (candidate2 == nums[i]) {
+                count2++;
+            } 
+            // If the current number is different from both candidates, decrement their counts.
+            else {
+                count1--;
+                count2--;
+            }
         }
-        vector<int> v;
-        for (auto it : a) 
-            if (it.second > floor(nums.size() / 3)) 
-                v.push_back(it.first);
-        return v;
+        vector<int> result;
+        int threshold = nums.size() / 3;
+        // Second pass to count occurrences of the potential majority elements.
+        count1 = 0, count2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (candidate1 == nums[i]) {
+                count1++;
+            } else if (candidate2 == nums[i]) {
+                count2++;
+            }
+        }
+        if (count1 > threshold) 
+            result.push_back(candidate1);
+        if (count2 > threshold) 
+            result.push_back(candidate2);
+        return result;
     }
+    // vector<int> majorityElement(vector<int>& nums) {
+    //     unordered_map<int, int> a;
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         a[nums[i]]++;
+    //     }
+    //     vector<int> v;
+    //     for (auto it : a) 
+    //         if (it.second > floor(nums.size() / 3)) 
+    //             v.push_back(it.first);
+    //     return v;
+    // }
 };
