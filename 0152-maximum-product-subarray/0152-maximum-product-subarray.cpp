a18a85c562ba628f8cd94294ee22ac256aa5a0c9
Aditya Kumar Singh <128071145+AdityaKrSingh26@users.noreply.github.com>
2024-12-25 10:38:19 +0530

    Time: 0 ms (100%), Space: 17.8 MB (10.05%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        if (nums.empty()) return 0;
-        
-        int maxProd = nums[0];
-        int minProd = nums[0];
-        int result = nums[0];
+        // =========== Prefix and suffix ============
+        int pre = 1, suff = 1;
+        int ans = INT_MIN;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (pre == 0)
+                pre = 1;
+            if (suff == 0)
+                suff = 1;
+            pre = pre * nums[i];
+            suff = suff * nums[n-i-1];
 
-        for (int i = 1; i < nums.size(); i++) {
-            // If the current element is negative, swap maxProd and minProd
-            if (nums[i] < 0) {
-                swap(maxProd, minProd);
-            }
-
-            // Update the maxProd and minProd
-            maxProd = max(nums[i], maxProd * nums[i]);
-            minProd = min(nums[i], minProd * nums[i]);
-
-            // Update the result to the maximum value encountered
-            result = max(result, maxProd);
+            ans = max(ans, max(pre, suff));
         }
+        return ans;
 
-        return result;
+        // if (nums.empty()) return 0;
+        // int maxProd = nums[0];
+        // int minProd = nums[0];
+        // int result = nums[0];
+        // for (int i = 1; i < nums.size(); i++) {
+        //     // If the current element is negative, swap maxProd and minProd
+        //     if (nums[i] < 0)
+        //         swap(maxProd, minProd);
+        //     // Update the maxProd and minProd
+        //     maxProd = max(nums[i], maxProd * nums[i]);
+        //     minProd = min(nums[i], minProd * nums[i]);
+        //     // Update the result to the maximum value encountered
+        //     result = max(result, maxProd);
+        // }
+        // return result;
     }
 };
